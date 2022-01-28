37abc549e5f23a545c4644dd6f5099bbbf77d70e
Monowar Hossain <monowar@fastsolutioninc.com>
2022-01-28 11:46:12 +0600

    Handling common exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,14 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Laravel\Passport\Exceptions\OAuthServerException;
+use Symfony\Component\ErrorHandler\Error\FatalError;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -13,7 +20,8 @@ class Handler extends ExceptionHandler
      * @var array<int, class-string<Throwable>>
      */
     protected $dontReport = [
-        //
+        OAuthServerException::class,
+        \League\OAuth2\Server\Exception\OAuthServerException::class,
     ];
 
     /**
@@ -37,5 +45,66 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    /**
+     * Report or log an exception.
+     *
+     * @param \Throwable $exception
+     *
+     * @return void
+     *
+     * @throws \Exception
+     */
+    public function report(Throwable $exception)
+    {
+        Log::critical($exception->getFile() . ' ' . $exception->getLine() . ' ' . $exception->getMessage());
+        Log::info(request()->fullUrl());
+        Log::info((array) request()->except(['password', 'pin']));
+
+        parent::report($exception);
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Throwable $exception
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     *
+     * @throws \Throwable
+     */
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof FatalError) {
+            return response()->json([
+                'code' => 500,
+                'messages' => ['Sorry! We are unable to process your request at this moment. Try again later'],
+                'data' => null,
+            ], 500);
+        }
+
+        return parent::render($request, $exception);
+    }
+
+    /**
+     * Convert an authentication exception into an unauthenticated response.
+     *
+     * @param Request $request
+     * @param AuthenticationException $exception
+     *
+     * @return RedirectResponse|JsonResponse
+     */
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        if ($request->expectsJson()) {
+            return response()->json([
+                'code' => 401,
+                'messages' => ['Authentication failed'],
+                'data' => null,
+            ], 401);
+        }
+        return redirect()->guest(route('login'));
     }
 }
